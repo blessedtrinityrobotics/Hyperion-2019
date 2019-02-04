4919b08a3c6184e5745dcd5e5643bf85e9d85548
Dario Zaccagnino <37509360+Dariozac05@users.noreply.github.com>
2019-02-04 18:55:19 -0500

    Constants and Gains java files

---
+++ Constants.java
@@ -0,0 +1,37 @@
+/**
+ * Simple class containing constants used throughout project
+ */
+package frc.robot;
+
+public class Constants {
+	/**
+	 * Set to zero to skip waiting for confirmation.
+	 * Set to nonzero to wait and report to DS if action fails.
+	 */
+	public final static int kTimeoutMs = 30;
+
+	/**
+	 * PID Gains may have to be adjusted based on the responsiveness of control loop.
+     * kF: 1023 represents output value to Talon at 100%, 6800 represents Velocity units at 100% output
+     * Not all set of Gains are used in this project and may be removed as desired.
+     * 
+	 * 	                                    			  kP   kI   kD   kF               Iz    PeakOut */
+	public final static Gains kGains_Distanc = new Gains( 0.1, 0.0,  0.0, 0.0,            100,  0.50 );
+    public final static Gains kGains_Elev    = new Gains( 1.0, 0.0,  0.0, 0.0,            500,  0.50 );
+	
+	/** ---- Flat constants, you should not need to change these ---- */
+	/* We allow either a 0 or 1 when selecting an ordinal for remote devices [You can have up to 2 devices assigned remotely to a talon/victor] */
+	public final static int REMOTE_0 = 0;
+	public final static int REMOTE_1 = 1;
+	/* We allow either a 0 or 1 when selecting a PID Index, where 0 is primary and 1 is auxiliary */
+	public final static int PID_PRIMARY = 0;
+	public final static int PID_TURN = 1;
+	/* Firmware currently supports slots [0, 3] and can be used for either PID Set */
+	public final static int SLOT_0 = 0;
+	public final static int SLOT_1 = 1;
+	public final static int SLOT_2 = 2;
+	public final static int SLOT_3 = 3;
+	/* ---- Named slots, used to clarify code ---- */
+	public final static int kSlot_Distanc = SLOT_0;
+	public final static int kSlot_Elev = SLOT_1;
+}
