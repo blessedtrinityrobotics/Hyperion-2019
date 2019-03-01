13516b4c60bfd94b79e41330d0f4a7a8c3a673d7
Dario Zaccagnino <37509360+Dariozac05@users.noreply.github.com>
2019-02-28 21:34:21 -0500

    limelight

--- PracticeBot.java
+++ PracticeBot.java
@@ -17,10 +17,13 @@ import edu.wpi.first.wpilibj.PWMSpeedController;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.hal.RelayJNI;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 
 
 
@@ -31,7 +34,7 @@ import edu.wpi.first.wpilibj.ADXRS450_Gyro;
  * creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class robot extends TimedRobot {
+public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
@@ -47,6 +50,10 @@ public class robot extends TimedRobot {
   Joystick JLeft = new Joystick(1);
   
   private ADXRS450_Gyro onboardGyro;
+  private boolean m_LimelightHasValidTarget = false;
+  private double m_LimelightDriveCommand = 0.0;
+  private double m_LimelightSteerCommand = 0.0;
+  
 
 
 
@@ -56,8 +63,8 @@ public class robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    //m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    //m_chooser.addOption("My Auto", kCustomAuto);
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
 
     //Gyro
@@ -109,6 +116,7 @@ public class robot extends TimedRobot {
    */
   @Override
   public void autonomousPeriodic() {
+    int counter = 0;
     switch (m_autoSelected) {
       case kCustomAuto:
         
@@ -126,28 +134,83 @@ public class robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     SmartDashboard.putNumber("Gyro:", onboardGyro.getAngle());
+    Update_Limelight_Tracking();
+      double driveForwardPower;
+      double turnPower;
+    //Drive Train Controls
+    if (JRight.getTrigger())
+        {
+          if (m_LimelightHasValidTarget)
+          {
+            //Limelight vision procesing control
+            SmartDashboard.putString("Valid Target", "True");
+            double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+            double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
+            SmartDashboard.putNumber("TX", tx);
+            SmartDashboard.putNumber("TA", ta);
+            driveForwardPower = m_LimelightDriveCommand;
+            turnPower = m_LimelightSteerCommand;
+            rightFirst.set(driveForwardPower-turnPower);
+            rightSecond.set(driveForwardPower-turnPower);
+            leftFirst.set(driveForwardPower+turnPower);
+            leftSecond.set(driveForwardPower+turnPower);
+          }
+          else
+          {
+            SmartDashboard.putString("Valid Target", "False");
+          }
+        }
+        else
+        {
+          rightFirst.set(-JRight.getY() - JRight.getX()/2);
+          rightSecond.set(-JRight.getY() - JRight.getX()/2);
+          leftFirst.set(-JRight.getY() + JRight.getX()/2);
+          leftSecond.set(-JRight.getY() + JRight.getX()/2);
+        }
     
-    
-    //Reset Gyro
-    if(JRight.getTrigger()){
+
+    if(JRight.getRawButton(2)){
       onboardGyro.reset();
     }
-    //Buttons for PID Controls
-    if(JRight.getRawButton(3)){ //Drive Straight Forward
-        PIDControl(0, 1.0);
-    } else if(JRight.getRawButton(2)){ //Drive Straight Backwards
-        PIDControl(0, -1.0);
-    } else if(JRight.getRawButton(4)){ //Turn Left 90 and continue driving straight
-        PIDControl(90, 1.0);
-    } else if(JRight.getRawButton(5)){ //Turn Right 90 and continue driving straight
-        PIDControl(-90, 1.0);
-    } else { //Arcade Drive
-        rightFirst.set(-JRight.getY() - JRight.getX()/2);
-        rightSecond.set(-JRight.getY() - JRight.getX()/2);
-        leftFirst.set(-JRight.getY() + JRight.getX()/2);
-        leftSecond.set(-JRight.getY() + JRight.getX()/2);
+
+    if(JRight.getRawButton(5)){
+      NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(1);
+      NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(1);
+
+    } 
+    if(JRight.getRawButton(4)){
+      NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(0);
+      NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(3);
+
     }
-}
+    if(JRight.getRawButton(3)){
+      double desiredAngle = 0;
+      double max_speed = 0.5;
+      double turn_kP = 0.015;
+      double curentAngle = onboardGyro.getAngle();
+      double turnCmd = (desiredAngle - curentAngle) * turn_kP;
+      SmartDashboard.putNumber("Error:", desiredAngle-curentAngle);
+      rightFirst.set(max_speed + turnCmd);
+      rightSecond.set(max_speed + turnCmd);
+      leftFirst.set(max_speed - turnCmd);
+      leftSecond.set(max_speed - turnCmd);
+    }
+
+    if(JLeft.getTrigger()){
+      double desiredAngle = 90;
+      double max_speed = 0.25;
+      double turn_kP = 0.005;
+      double curentAngle = onboardGyro.getAngle();
+      double turnCmd = (desiredAngle - curentAngle) * turn_kP;
+      SmartDashboard.putNumber("Error:", desiredAngle-curentAngle);
+      rightFirst.set(max_speed + turnCmd);
+      rightSecond.set(max_speed + turnCmd);
+      leftFirst.set(max_speed - turnCmd);
+      leftSecond.set(max_speed - turnCmd);
+    }
+    
+  }
+
   /**
    * This function is called periodically during test mode.
    */
@@ -155,25 +218,44 @@ public class robot extends TimedRobot {
   public void testPeriodic() {
   }
 
-/** 
-*
-* @param angle target angle
-* @param direction forward or backwards; 1.0 for forward, and -1.0 for backwards
-*/
-  public void PIDControl(int angle, double direction){
-    double desiredAngle = angle;
-    double max_speed = direction * 0.5;
-    double turn_kP = 0.015;
-    double turn_kI = 0;
-    double integral = 0;
-    double curentAngle = onboardGyro.getAngle();
-    double error = desiredAngle - curentAngle;
-    integral = integral + (error*0.02);
-    double turnCmd = (error * turn_kP) + (turn_kI * integral) ;
-    SmartDashboard.putNumber("Error:", error);
-    rightFirst.set(max_speed + turnCmd);
-    rightSecond.set(max_speed + turnCmd);
-    leftFirst.set(max_speed - turnCmd);
-    leftSecond.set(max_speed - turnCmd);
-  }
+  /**
+   * This function implements a simple method of generating driving and steering commands
+   * based on the tracking data from a limelight camera.
+   */
+  public void Update_Limelight_Tracking()
+  {
+        // These numbers must be tuned for your Robot!  Be careful!
+        final double STEER_K = 0.02;              // how hard to turn toward the target
+        final double DRIVE_K = 0.1;                    // how hard to drive fwd toward the target
+        final double DESIRED_TARGET_AREA = 3.5;       // Area of the target when the robot reaches the wall
+        final double MAX_DRIVE = 0.75;                   // Simple speed limit so we don't drive too fast
+
+        double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+        double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+        double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
+
+        if (tv < 1.0)
+        {
+          m_LimelightHasValidTarget = false;
+          m_LimelightDriveCommand = 0.0;
+          m_LimelightSteerCommand = 0.0;
+          return;
+        }
+
+        m_LimelightHasValidTarget = true;
+
+        // Start with proportional steering
+        double steer_cmd = tx * STEER_K;
+        m_LimelightSteerCommand = steer_cmd;
+
+        // try to drive forward until the target area reaches our desired area
+        double drive_cmd = (DESIRED_TARGET_AREA - ta) * DRIVE_K;
+
+        // don't let the robot drive too fast into the goal
+        if (drive_cmd > MAX_DRIVE)
+        {
+          drive_cmd = MAX_DRIVE;
+        }
+        m_LimelightDriveCommand = drive_cmd;
+    }
 }
